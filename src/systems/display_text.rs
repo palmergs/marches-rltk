04350b8376ca9b50704bc45947a2436ff5a2e822
Galen <palmergs@gmail.com>
2021-01-02 21:13:45 -0500

    fixed fade text direction

--- src/systems/display_text.rs
+++ src/systems/display_text.rs
@@ -9,13 +9,13 @@ pub fn display_text(
 ) {
     if cmd.count < cmd.ticks {
         let fade = cmd.count as f32 / cmd.ticks as f32;
-        cmd.count += 1;        
+        cmd.count += 1;
         match cmd.display {
             TextDisplay::AnimateUp(pt) => {
                 if camera.in_central_view(pt) {
                     let mut draw_batch = DrawBatch::new();
                     draw_batch.target(UI_LAYER);
-                    let dy = 4 - (fade * 4.0) as i32;
+                    let dy = (fade * 4.0) as i32;
                     let pt = pt - camera.offset() - Point::new(0, dy);
                     let rgba = cmd.color - RGBA::from_f32(0., 0., 0., fade);
                     draw_batch.print_color(pt, cmd.text.clone(), ColorPair::new(rgba, BLACK));
@@ -26,15 +26,14 @@ pub fn display_text(
                 if camera.in_central_view(pt) {
                     let mut draw_batch = DrawBatch::new();
                     draw_batch.target(UI_LAYER);
-                    let dy = 4 - (fade * 4.0) as i32;
-                    let pt = pt - camera.offset() - Point::new(0, dy);
+                    let pt = pt - camera.offset();
                     let rgba = cmd.color - RGBA::from_f32(0., 0., 0., fade);
                     draw_batch.print_color(pt, cmd.text.clone(), ColorPair::new(rgba, BLACK));
                     draw_batch.submit(4000).expect("error rendering fading text");
                 }
             },
         }
-            
+
     } else {
         commands.remove(*entity);
     }
